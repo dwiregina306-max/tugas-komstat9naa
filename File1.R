269b358890c291763fec36456f03db07838667d9
Regina <dwiregina306@gmail.com>
2026-05-15 10:51:26 +0700

    "Nambah visualisasi melalui plot"

--- File1.R
+++ File1.R
@@ -4,4 +4,11 @@
 data(mtcars)
 head(mtcars)
 
+#Udah di branch baru
+#Visual data mtcars
+plot(mtcars$wt, mtcars$mpg,
+     main = "Hubungan Berat Mobil dan MPG",
+     xlab = "Berat Mobil (1000 lbs)",
+     ylab = "Miles/(US) gallon",
+     col = "blue", pch = 19)
 
